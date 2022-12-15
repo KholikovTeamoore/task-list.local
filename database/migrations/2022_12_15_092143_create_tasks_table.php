ddebb4d102fcadca285ed5057547cd35a1574c59
? <106308741+KholikovTeamoore@users.noreply.github.com>
2022-12-15 09:34:55 +0000

    Created Task model and migration

---
+++ database/migrations/2022_12_15_092143_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('user_id')->unsigned()->comment('Пользователь');
+            $table->string('name')->comment('Название задачи');
+            $table->integer('priority')->unsigned()->comment('Приоритет задачи');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
