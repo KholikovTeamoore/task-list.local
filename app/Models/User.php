72bfa043f60167ce616a804fb601b7a75ecb124a
? <106308741+KholikovTeamoore@users.noreply.github.com>
2022-12-15 10:43:29 +0000

    Create model User and Task relationships one to many

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,21 +1,47 @@
 <?php
 
+/**
+ * Список задач
+ *
+ * PHP version 7
+ *
+ * @category App
+ * @package  Models
+ * @author   Student <student@example.com>
+ * @license  http://unlicense.org/ Unlicense
+ * @link     http://php.net
+ */
+
+
 namespace App\Models;
 
+use App\Models\Task;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Модель User
+ *
+ * PHP version 7
+ *
+ * @category App
+ * @package  Models
+ * @author   Student <student@example.com>
+ * @license  http://unlicense.org/ Unlicense
+ * @version  Release: @package_version@
+ * @link     http://php.net
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Атрибуты, для которых разрешено массовое присвоение значений..
      *
-     * @var array<int, string>
+     * @var array <int, string>
      */
     protected $fillable = [
         'name',
@@ -24,9 +50,9 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * Атрибуты, которые должны быть скрыты из массивов.
      *
-     * @var array<int, string>
+     * @var array <int, string>
      */
     protected $hidden = [
         'password',
@@ -34,11 +60,21 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * Атрибуты, которые должны быть типизированы.
      *
-     * @var array<string, string>
+     * @var array <string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Получить задачи пользователя
+     *
+     * @return Task
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,7 +42,7 @@ class DatabaseSeeder extends Seeder
         $this->call(
             [
                 UsersTableSeeder::class,
-                TasksTableSeeder::class,
+                // TasksTableSeeder::class,
             ]
         );
     }
